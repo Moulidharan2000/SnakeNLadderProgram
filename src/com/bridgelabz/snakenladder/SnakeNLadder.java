916628ad8a6db4abbe40b3139d23d84002abcd1c
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-05 13:22:49 +0530

    Added : Repeat Till Player Reach Winning Position

--- src/com/bridgelabz/snakenladder/SnakeNLadder.java
+++ src/com/bridgelabz/snakenladder/SnakeNLadder.java
@@ -12,32 +12,43 @@ public class SnakeNLadder {
 		System.out.print("Enter the Number of Player : ");
 		int player = scan.nextInt();
 		int position = 0;
+		int WIN_POSITION = 100;
 		
 		if(player == 1) {
-			int dice = 1+randomNum.nextInt(6);
 			System.out.println("Player : "+player);
-			System.out.println("Starting Position : "+position);
-			System.out.println("Dice Number : "+dice);
-			
-			int options = randomNum.nextInt(3);
-			
-			switch(options) {
-				case 1 :
-					System.out.println("Player Gets Ladder !!!");
-					System.out.println("Player Position : "+(position+dice));
-					break;
+			System.out.println("Starting Position : "+position+"\n");
 				
-				case 2 :
-					System.out.println("Player Bit by Snake Ladder !!!");
-					System.out.println("Player Position : "+(position-dice));
-					break;
+			while(position <= WIN_POSITION) {
+
+				int dice = 1+randomNum.nextInt(6);
+				System.out.println("Dice Number : "+dice+"\n");
+				int options = randomNum.nextInt(3);
 				
-				default :
-					System.out.println("No Play");
-					System.out.println("Player Position : "+position);
-					break;
+				switch(options) {
+					case 1 :
+						position = position + dice;
+						System.out.println("Player Gets Ladder !!!");
+						System.out.println("Player Position : "+position+"\n");
+						break;
+					
+					case 2 :
+						position = position - dice;
+						System.out.println("Player Bit by Snake Ladder !!!");
+						if(position <= 0) {
+							position = 0;
+							System.out.println("Player Position : "+position+"\n");
+						}
+						else
+							System.out.println("Player Position : "+position+"\n");
+						break;
+					
+					default :
+						System.out.println("No Play");
+						System.out.println("Player Position : "+position+"\n");
+						break;
+					}
+				}
 			}
-		}
 		else
 			System.out.println("Play with Single Player!!!");
 	}
